e147aba43435783e10bd29ef3c947849b1f2f8d3
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-09-18 13:40:16 -0300

    feat: add CustomButton to LoginPage

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,26 +1,43 @@
 /* eslint-disable react/react-in-jsx-scope */
 
+import { BsGoogle } from "react-icons/bs";
+import { FiLogIn } from "react-icons/fi";
+
+import CustomButton from "../../components/custom-button/CustomButtonComponent";
+import Header from "../../components/header/HeaderComponent";
+
 const LoginPage = () => {
   return (
-    <div className="flex h-full items-center justify-center">
-      <div className="flex w-[450px] flex-col items-center">
-        <h1 className="mb-5 text-xl font-semibold text-red-500">
-          Entre com a sua conta
-        </h1>
-        <p className="my-5 mt-5 w-full border-b border-b-red-500 pb-5 text-center font-medium text-red-500">
-          ou entre com o seu e-mail
-        </p>
-        <div className="mb-5 w-full">
-          <label>Email</label>
-          <input type="text" />
-        </div>
+    <>
+      <Header />
+      <div className="flex h-full items-center justify-center">
+        <div className="flex w-[450px] flex-col items-center">
+          <h1 className="text-color-secundary mb-5 text-xl font-semibold">
+            Entre com a sua conta
+          </h1>
 
-        <div className="mb-1 w-full font-semibold">
-          <label>Senha</label>
-          <input type="password" />
+          <CustomButton startIcon={<BsGoogle size={18} />}>
+            Entrar com o Google
+          </CustomButton>
+          <p className="border-b-color-secundary text-color-secundary my-5 mt-5 w-full border-b pb-5 text-center font-medium">
+            ou entre com o seu e-mail
+          </p>
+          <div className="mb-5 w-full">
+            <label>Email</label>
+            <input type="text" />
+          </div>
+
+          <div className="mb-1 w-full font-semibold">
+            <label>Senha</label>
+            <input type="password" />
+
+            <CustomButton startIcon={<FiLogIn size={18} />}>
+              Entrar
+            </CustomButton>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
