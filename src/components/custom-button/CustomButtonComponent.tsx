2c859fc98c6dcd5b5347093cbf55a21f5e2dfe2e
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-09-18 13:36:38 -0300

    feat: add Custom Button Component

---
+++ src/components/custom-button/CustomButtonComponent.tsx
@@ -0,0 +1,26 @@
+import React, { ButtonHTMLAttributes } from "react";
+
+interface CustomButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  startIcon?: React.ReactNode;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  children,
+  startIcon,
+  ...rest
+}) => {
+  return (
+    <button
+      {...rest}
+      className="bg-color-secundary shadow-outiline-dark flex w-full items-center justify-center rounded-lg border-none bg-[#000] px-4 py-2 font-semibold text-white transition-all hover:cursor-pointer"
+    >
+      {startIcon && (
+        <div className="mr-2 flex h-full items-center">{startIcon}</div>
+      )}
+      {children}
+    </button>
+  );
+};
+
+export default CustomButton;
