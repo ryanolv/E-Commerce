4c9aa63fbebaedaac4a0afa4caf124f56b396f8f
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-09-18 11:22:24 -0300

    fix: update Category Item background image

--- src/components/categoryItem/CategoryItemComponent.tsx
+++ src/components/categoryItem/CategoryItemComponent.tsx
@@ -2,28 +2,22 @@ import React from "react";
 
 import Category from "../../types/category-types";
 
-interface ProductsProps {
-  id: string;
-  imageUrl: string;
-  name: string;
-  price: number;
-}
+import "./category-item-styles.css";
 
 interface CategoryItemProps {
   category: Category;
-  products: ProductsProps[];
 }
 
-const CategoryItemComponent: React.FC<CategoryItemProps> = ({ category }) => {
+const CategoryItem: React.FC<CategoryItemProps> = ({ category }) => {
   const bgImage = category.imageUrl;
 
   return (
     <div
-      style={{ backgroundImage: bgImage }}
-      className="shadow-outline-dark flex h-full w-full items-center justify-center gap-4 rounded-xl bg-cover bg-center bg-repeat"
+      style={{ backgroundImage: `url('${bgImage}')` }}
+      className="category-item-container"
       key={category.id}
     >
-      <div className="color-[#f8f9fa] bg-text hover:bg-color-hover rounded-xl border-2 border-solid border-[#212529] pb-3 pl-8 pr-8 pt-3 text-center transition-shadow hover:cursor-pointer">
+      <div className="category-name">
         <p>{category.displayName}</p>
         <p>Explorar</p>
       </div>
@@ -31,4 +25,4 @@ const CategoryItemComponent: React.FC<CategoryItemProps> = ({ category }) => {
   );
 };
 
-export default CategoryItemComponent;
+export default CategoryItem;
