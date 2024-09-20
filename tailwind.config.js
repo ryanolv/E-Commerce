8c584b570b7394de613791916d89de3ce31297b0
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-09-19 21:00:12 -0300

    feat: add tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      color: {
-        text: "rgba(233, 236, 239, 0.45)",
-        hover: "rgba(233, 236, 239, 0.75)",
-        secundary: "rgba(0, 0, 0, 0.3)",
+      colors: {
+        primary: "#212529",
+        whiteText: "#F8F9FA",
+        darkText: "#343A40",
+        bgDark: "#212529",
+        bgWhite: "#F8F9FA",
+        inputBg: "#E9ECEF",
+        inputPlaceholder: "#6C757D",
+        error: "#FF6A6A",
       },
       shadow: {
         "outline-dark": "2px 2px 2px rgba(0, 0, 0, 0.25)",
