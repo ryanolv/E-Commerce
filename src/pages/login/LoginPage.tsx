b32641743231ad79a6751724c0f2bd615b84952b
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-09-19 21:03:02 -0300

    feat: add CustomInput to LoginPage

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -5,6 +5,7 @@ import { FiLogIn } from "react-icons/fi";
 
 import CustomButton from "../../components/custom-button/CustomButtonComponent";
 import Header from "../../components/header/HeaderComponent";
+import CustomInput from "../../components/custom-input/CustomInputComponent";
 
 const LoginPage = () => {
   return (
@@ -12,29 +13,31 @@ const LoginPage = () => {
       <Header />
       <div className="flex h-full items-center justify-center">
         <div className="flex w-[450px] flex-col items-center">
-          <h1 className="text-color-secundary mb-5 text-xl font-semibold">
+          <h1 className="text-color-darkText mb-5 text-xl font-semibold">
             Entre com a sua conta
           </h1>
 
           <CustomButton startIcon={<BsGoogle size={18} />}>
             Entrar com o Google
           </CustomButton>
-          <p className="border-b-color-secundary text-color-secundary my-5 mt-5 w-full border-b pb-5 text-center font-medium">
+          <p className="border-b-color-bgDark text-color-textDark my-5 mt-5 w-full border-b pb-5 text-center font-medium">
             ou entre com o seu e-mail
           </p>
           <div className="mb-5 w-full">
-            <label>Email</label>
-            <input type="text" />
+            <CustomInput
+              label="E-mail"
+              type="email"
+              placeholder="Digite o seu e-mail"
+            />
           </div>
-
-          <div className="mb-1 w-full font-semibold">
-            <label>Senha</label>
-            <input type="password" />
-
-            <CustomButton startIcon={<FiLogIn size={18} />}>
-              Entrar
-            </CustomButton>
+          <div className="mb-5 w-full">
+            <CustomInput
+              label="Senha"
+              type="password"
+              placeholder="Digite sua senha"
+            />
           </div>
+          <CustomButton startIcon={<FiLogIn size={18} />}>Entrar</CustomButton>
         </div>
       </div>
     </>
