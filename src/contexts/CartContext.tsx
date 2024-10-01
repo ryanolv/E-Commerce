2e86fe145635aaa91827c6bd7899adeb9514e122
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-09-30 22:09:45 -0300

    feat: add CartContext

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, FunctionComponent, useState } from "react";
+
+import CartProduct from "../types/cart-types";
+
+interface ICartContext {
+  isVisible: boolean;
+  products: CartProduct[];
+  toggleCart: () => void;
+}
+
+interface ICartContextProvider {
+  children: React.ReactNode;
+}
+
+const CartContext = createContext<ICartContext>({
+  isVisible: false,
+  products: [],
+  toggleCart: () => {},
+});
+
+const CartContextProvider: FunctionComponent<ICartContextProvider> = ({
+  children,
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [products] = useState<CartProduct[]>([]);
+
+  const toggleCart = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <CartContext.Provider value={{ isVisible, products, toggleCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartContextProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import { UserContextProvider } from "./contexts/UserContext.tsx";
+import CategoryContextProvider from "./contexts/CategoryContext.tsx";
+import CartContextProvider from "./contexts/CartContext.tsx";
 
 import App from "./App.tsx";
 import "./index.css";
-import CategoryContextProvider from "./contexts/CategoryContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <UserContextProvider>
       <CategoryContextProvider>
-        <App />
+        <CartContextProvider>
+          <App />
+        </CartContextProvider>
       </CategoryContextProvider>
     </UserContextProvider>
   </StrictMode>,
