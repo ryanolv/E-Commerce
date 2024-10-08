51416f796ddfc62c61d9a5dfe8e937284cdad46a
Ryan Oliveira <ryanprofissional02@gmail.com>
2024-10-07 22:52:39 -0300

    feat: add stripe api integration to checkout

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,22 +1,40 @@
-import React, { FunctionComponent, useContext } from "react";
+import React, { FunctionComponent, useContext, useState } from "react";
 import { BsBagCheck } from "react-icons/bs";
+import axios from "axios";
 
 import { CartContext } from "../contexts/CartContext";
 
 import CustomButton from "./custom-button/CustomButtonComponent";
 import CartItem from "./CartItem";
+import Loading from "./loading/Loading";
 
 const Checkout: FunctionComponent = () => {
   const { products, productsTotalPrice } = useContext(CartContext);
+  const [isLoading, setIsloading] = useState(false);
+  const handleFinishPushaseClick = async () => {
+    try {
+      setIsloading(true);
+      const apiUrl = process.env.REACT_APP_API_URL;
+      const { data } = await axios.post(`${apiUrl}/create-checkout-session`, {
+        products,
+      });
+      window.location.href = data.url;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsloading(false);
+    }
+  };
 
   return (
     <>
       <div className="flex flex-col items-center overflow-hidden py-8">
+        {isLoading && <Loading message="Finalizando compra..." />}
         <p className="text-2xl font-bold">Checkout</p>
 
         {products.length > 0 ? (
           <>
-            <div className="scrollbar-thin scrollbar-thumb-darkText my-4 min-w-[650px] overflow-y-scroll">
+            <div className="my-4 min-w-[650px] overflow-y-scroll scrollbar-thin scrollbar-thumb-darkText">
               {products.map((product) => (
                 <CartItem key={product.id} product={product} />
               ))}
@@ -24,7 +42,11 @@ const Checkout: FunctionComponent = () => {
             <p className="mb-4 w-[650px] text-xl font-semibold">
               Total: R${productsTotalPrice}
             </p>
-            <CustomButton className="w-[650px]" startIcon={<BsBagCheck />}>
+            <CustomButton
+              className="w-[650px]"
+              startIcon={<BsBagCheck />}
+              onClick={handleFinishPushaseClick}
+            >
               Finalizar Compra
             </CustomButton>
           </>
